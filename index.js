106e8be5a21d62d793f6718ed4ca245e9abb7835
Kenan Yildirim <kenan@kenany.me>
2021-04-01 17:16:08 -0400

    fix: add JSDoc

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var randomOctet = require('secure-random-octet');
 
+/**
+ * Generates a secure random byte string of a specified length.
+ *
+ * @param {number} length String length.
+ * @returns {string} String of random bytes.
+ */
 function randomBytes(length) {
   var result = '';
   for (var i = 0; i < length; i++) {
